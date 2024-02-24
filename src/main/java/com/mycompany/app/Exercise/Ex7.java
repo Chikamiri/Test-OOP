491458b2efdc78fffc0e61b60d10d5789f7b590b
Chikamiri <bach.px@outlook.com>
2024-02-24 16:28:33 +0700

    New files and small configs. Exercise 7 and 9. Small changes in pom.xml to get rid of 2 annoying warmings.

---
+++ src/main/java/com/mycompany/app/Exercise/Ex7.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.Exercise;
+
+public class Ex7 {
+    public static void main(String[]args){
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+            if(i==99)break;
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/app/Exercise/Ex9.java
@@ -0,0 +1,27 @@
+package com.mycompany.app.Exercise;
+import java.util.*;
+
+public class Ex9{
+    static int calc(int n){
+        int f1=0, f2=1, fn=1;
+        if(n<0){
+            return -1;
+        } else if(n==0||n==1){
+            return n;
+        } else{
+            for (int i=2; i<n; i++) {
+                f1=f2;
+                f2=fn;
+                fn=f1 +f2;
+            }
+        }
+        return fn;
+    }
+    public static void main(String[] args){
+        Scanner temp = new Scanner(System.in);
+        int n = temp.nextInt();
+        
+        for (int i=1; i<=n; i++)System.out.println(calc(i));
+        temp.close();
+    }
+}
