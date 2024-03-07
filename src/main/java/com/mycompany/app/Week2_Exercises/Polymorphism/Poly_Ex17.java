bbf5c14a343e27fcd732f7cad0099138ae4626f1
Chikamiri <bach.px@outlook.com>
2024-03-07 17:43:03 +0700

    Fix failed build due to duplicate class in Ex17

--- src/main/java/com/mycompany/app/Week2_Exercises/Polymorphism/Poly_Ex17.java
+++ src/main/java/com/mycompany/app/Week2_Exercises/Polymorphism/Poly_Ex17.java
@@ -3,14 +3,14 @@ package com.mycompany.app.Week2_Exercises.Polymorphism;
 public class Poly_Ex17 {
     public static void main(String[] args) {
         Cycle[] cycles = new Cycle[]{ new Unicycle(), new Tricycle(), new Bicycle() };
-        ((Unicycle)cycles[0]).balance();
-        ((Bicycle)cycles[1]).balance();
+        ((UnicycleEx17)cycles[0]).balance();
+        ((BicycleEx17)cycles[1]).balance();
     }
 }
-class Unicycle extends Cycle {
+class UnicycleEx17 extends Cycle {
     public void balance(){}
 }
-class Bicycle extends Cycle {
+class BicycleEx17 extends Cycle {
     public void balance(){}
 }
     
